b767899549ef2938ec49bf2238567fb68c2caaf3
talalzeini <talalzeini@gmail.com>
2021-07-09 19:39:05 -0700

    started and finished project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+function countingFunction() {
+  return 10;
+}
 
 function App() {
+  var [count, setCount] = useState(() => countingFunction());
+
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <button onClick={increment}>+</button>
+        <p>{count}</p>
+        <button onClick={decrement}>-</button>
+      </div>
     </div>
   );
 }
